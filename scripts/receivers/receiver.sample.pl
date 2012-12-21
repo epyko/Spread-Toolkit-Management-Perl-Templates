8206f93931fa27be73e6e529fcbe554b6313c2c0
Ferran Epifanio <ferran@epyko.epyko.com>
2012-12-21 17:33:29 +0900

    fixed tabs

--- scripts/receivers/receiver.sample.pl
+++ scripts/receivers/receiver.sample.pl
@@ -7,21 +7,21 @@ use Spread::Messaging::Exception;
 do"../../conf/receivers/receiver.sample.conf";
 require"../../scripts/receivers/custom.sample.pm";
 sub put_output {
-        $spread->recv();
-        $message = (ref($spread->message) eq "ARRAY" ? join(','< @{$spread->message}) : $spread->message);
-        &custom::test($message);
+    $spread->recv();
+    $message = (ref($spread->message) eq "ARRAY" ? join(','< @{$spread->message}) : $spread->message);
+    &custom::test($message);
 }
 eval{
-        $spread = Spread::Messaging::Content->new();
-        $spread->join_group("$group");
-        Event->io(fd => $spread->fd, cb => \&put_output);
-        Event::loop();
+    $spread = Spread::Messaging::Content->new();
+    $spread->join_group("$group");
+    Event->io(fd => $spread->fd, cb => \&put_output);
+    Event::loop();
 };if (my $ex = $@) {
-        my $ref = ref($ex);
-        if ($ref && $ex->isa('Spread::Messaging::Exception')) {
-                $ex->errno;
-                printf("Error: %s cased by: %s\n", $ex->errno, $ex->errstr);
-        } else {
-                warn $@;
-        }
+    my $ref = ref($ex);
+    if ($ref && $ex->isa('Spread::Messaging::Exception')) {
+        $ex->errno;
+        printf("Error: %s cased by: %s\n", $ex->errno, $ex->errstr);
+    } else {
+        warn $@;
+    }
 }
